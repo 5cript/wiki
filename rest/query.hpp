c4a2f483635efb3034ff91f1123a91bc6069fdc4
5cript <Tim06TR@gmail.com>
2016-01-31 16:46:54 +0100

    Updated code to comply with new SimpleJSON names.

--- rest/query.hpp
+++ rest/query.hpp
@@ -2,12 +2,12 @@
 #define QUERY_HPP_INCLUDED
 
 #ifndef Q_MOC_RUN // A Qt workaround, for those of you who use Qt
-#   include "../SimpleJSON/parse/jsd.h"
-#   include "../SimpleJSON/parse/jsd_convenience.h"
-#   include "../SimpleJSON/parse/jsd_optional.h"
-#   include "../SimpleJSON/stringify/jss.h"
-#   include "../SimpleJSON/stringify/jss_optional.h"
-#   include "../SimpleJSON/stringify/jss_fusion_adapted_struct.h"
+#   include "SimpleJSON/parse/jsd.h"
+#   include "SimpleJSON/parse/jsd_convenience.h"
+#   include "SimpleJSON/parse/jsd_optional.h"
+#   include "SimpleJSON/stringify/jss.h"
+#   include "SimpleJSON/stringify/jss_optional.h"
+#   include "SimpleJSON/stringify/jss_fusion_adapted_struct.h"
 #endif
 
 #include <string>
@@ -21,29 +21,29 @@ namespace WikiClient
     struct Page;
     struct Revision;
 
-    struct Query : public JSON::FusionStruct <Query>
-                 , public JSON::ParsableStruct <Query>
+    struct Query : public JSON::Stringifiable <Query>
+                 , public JSON::Parsable <Query>
     {
         std::map <std::string, Page> pages;
     };
 
-    struct QueryBatch : public JSON::FusionStruct <QueryBatch>
-                      , public JSON::ParsableStruct <QueryBatch>
+    struct QueryBatch : public JSON::Stringifiable <QueryBatch>
+                      , public JSON::Parsable <QueryBatch>
     {
         std::string batchcomplete;
         boost::optional <Query> query;
     };
 
-    struct Revision : public JSON::FusionStruct <Revision>
-                    , public JSON::ParsableStruct <Revision>
+    struct Revision : public JSON::Stringifiable <Revision>
+                    , public JSON::Parsable <Revision>
     {
         std::string contentformat;
         std::string contentmodel;
         JSON::rename <std::string, SHORT_STRING("*")> content;
     };
 
-    struct Page : public JSON::FusionStruct <Page>
-                , public JSON::ParsableStruct <Page>
+    struct Page : public JSON::Stringifiable <Page>
+                , public JSON::Parsable <Page>
     {
         uint64_t pageid;
         uint64_t ns;
